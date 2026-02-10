fd382d49af7d990b700b3cc9fd2ab9e784f1a7db
Evila <66744375+evi-nunes@users.noreply.github.com>
2026-02-09 21:50:38 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,28 +5,31 @@ const botaoMostrar = document.getElementById("mostrar");
 const botaoLimpar = document.getElementById("limpar");
 const botaoCor = document.getElementById("cor");
 
+let azul = false; // controla a cor (estado)
+
+// Mostrar texto
 botaoMostrar.addEventListener("click", function () {
   if (input.value === "") {
     resultado.textContent = "Digite algo 🙂";
     return;
   }
+
   resultado.textContent = input.value;
 });
 
+// Limpar tudo
 botaoLimpar.addEventListener("click", function () {
   resultado.textContent = "";
   input.value = "";
 });
 
-let azul = false;
-
+// Mudar cor (alternando)
 botaoCor.addEventListener("click", function () {
   if (azul) {
     resultado.style.color = "black";
   } else {
     resultado.style.color = "blue";
   }
-  azul = !azul;
-});
 
+  azul = !azul; // inverte a cor
 });
