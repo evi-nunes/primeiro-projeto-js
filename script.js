9b3c031f9370f8c668fe52dc3be98a0ace73994a
Evila <66744375+evi-nunes@users.noreply.github.com>
2026-02-09 21:34:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("JS carregou");
+const input = document.getElementById("texto");
+const botao = document.getElementById("botao");
+const resultado = document.getElementById("resultado");
+
+botao.addEventListener("click", function () {
+  resultado.textContent = input.value;
+});
+
