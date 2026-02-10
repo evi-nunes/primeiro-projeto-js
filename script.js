3154f6e69ce23dd04586ccdb2809fe674a867263
Evila <66744375+evi-nunes@users.noreply.github.com>
2026-02-09 21:41:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const input = document.getElementById("texto");
-const botao = document.getElementById("botao");
 const resultado = document.getElementById("resultado");
 
-botao.addEventListener("click", function () {
+const botaoMostrar = document.getElementById("mostrar");
+const botaoLimpar = document.getElementById("limpar");
+const botaoCor = document.getElementById("cor");
+
+botaoMostrar.addEventListener("click", function () {
+  if (input.value === "") {
+    resultado.textContent = "Digite algo 🙂";
+    return;
+  }
   resultado.textContent = input.value;
 });
 
+botaoLimpar.addEventListener("click", function () {
+  resultado.textContent = "";
+  input.value = "";
+});
+
+botaoCor.addEventListener("click", function () {
+  resultado.style.color = "blue";
+});
